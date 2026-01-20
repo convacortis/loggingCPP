5e0ee66d004c32498138a78bbcb9927139f05859
convacortis <convacortis@proton.me>
2026-01-20 17:24:07 +0000

    added color

--- src/logging.h
+++ src/logging.h
@@ -1,5 +1,23 @@
 #pragma once
 
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"                 /* Black */
+#define RED     "\033[31m"                 /* Red */
+#define GREEN   "\033[32m"                 /* Green */
+#define YELLOW  "\033[33m"                 /* Yellow */
+#define BLUE    "\033[34m"                 /* Blue */
+#define MAGENTA "\033[35m"                 /* Magenta */
+#define CYAN    "\033[36m"                 /* Cyan */
+#define WHITE   "\033[37m"                 /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
 class log
 {
 public:
@@ -20,17 +38,17 @@ public:
     void fatal(char* msg)
     {
         
-        std::cout << getTimestamp() << "[FATAL] " << msg << std::endl;
+        std::cout << RED << getTimestamp() << "[FATAL] " << msg << std::endl;
     }
 
     void error(char* msg)
     {
-        std::cout << getTimestamp() << "[ERROR] " << msg << std::endl;
+        std::cout << MAGENTA << getTimestamp() << "[ERROR] " << msg << std::endl;
     }
 
     void warn(char* msg)
     {
-        std::cout << getTimestamp() << "[WARN]" << msg << std::endl;
+        std::cout << YELLOW << getTimestamp() << "[WARN]" << msg << std::endl;
     }
 
     void info(char* msg)
